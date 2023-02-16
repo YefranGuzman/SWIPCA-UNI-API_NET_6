c8a8450d7637ec407f6f1563cbd4fd239e701987
Yefran Jose Guzman Rivas <Yefran.Guzman319@std.uni.edu.ni>
2023-02-16 00:10:02 -0600

    Se agrega el model de Horario,Grupo e Historial

---
+++ SWIPCA-UNI-API/Models/Historial.cs
@@ -0,0 +1,14 @@
+﻿namespace SWIPCA_UNI_API.Models
+{
+    public class Historial
+    {
+        public string? UsuarioCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public string? UsuarioModificacion { get; set; }
+        public DateTime? FechaModificacion { get; set; }
+        public int idHorario { get; set; }
+        public int idCarrera { get; set; }
+        public int idClase { get; set; }
+        public int idGrupo { get; set; }
+    }
+}
