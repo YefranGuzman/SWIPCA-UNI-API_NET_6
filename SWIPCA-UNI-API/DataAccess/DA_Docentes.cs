072a677544f67a7ceefc783c43e1ea1c1b7c46e1
Yefran Jose Guzman Rivas <Yefran.Guzman319@std.uni.edu.ni>
2023-03-04 17:58:06 -0600

    Se agrega DA_Para obtener Docentes

---
+++ SWIPCA-UNI-API/DataAccess/DA_Docentes.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWIPCA_UNI_API.Models;
+
+namespace SWIPCA_UNI_API.DataAccess
+{
+    public class DA_Docentes
+    {
+   
+        DbCargaAcademicaContext db = new DbCargaAcademicaContext();
+
+        public async Task<List<string>> ObtenerDocentes(int idDepartamento, int idFacultad)
+        {
+            var listaDocentes = await (from a in db.Docentes
+                                       join b in db.Departamentos
+                                       on a.IdDepartamento equals b.IdDepartamento
+                                       join c in db.Usuarios
+                                       on a.IdUsuario equals c.IdUsuario
+                                       join f in db.Facultads
+                                       on b.IdFacultad equals f.IdFacultad
+                                       where a.Estado == 1 && b.IdDepartamento == idDepartamento && b.IdFacultad == idFacultad
+                                       select c.PrimerNombre + " " + c.SegundoNombre + " " + c.PrimerApellido + " " + c.SegundoApellido).ToListAsync();
+            return listaDocentes;
+        }
+
+
+    }
+}
