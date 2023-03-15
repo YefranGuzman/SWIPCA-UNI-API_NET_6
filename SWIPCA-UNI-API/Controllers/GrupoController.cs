9f5d8e8536b699f0188ce09ef69e1366c00af6a7
Yefran Jose Guzman Rivas <Yefran.Guzman319@std.uni.edu.ni>
2023-03-15 16:50:34 -0500

    Se agrega el DA y Controller de Grupos

---
+++ SWIPCA-UNI-API/Controllers/GrupoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SWIPCA_UNI_API.DataAccess;
+using SWIPCA_UNI_API.Models;
+
+namespace SWIPCA_UNI_API.Controllers
+{
+    public class GrupoController : Controller
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Grupo>>> GetObtenerGrupo()
+        {
+            var DA_Grupos = new DA_Grupo();
+            var result = DA_Grupos.ObtenerGrupo();
+
+            return Ok(result);
+        }
+    }
+}
