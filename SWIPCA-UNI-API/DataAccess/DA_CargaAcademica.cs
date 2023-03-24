58582d1e57cce10bb526811d12f4919cb8241ce9
Yefran Jose Guzman Rivas <Yefran.Guzman319@std.uni.edu.ni>
2023-03-23 19:44:26 -0500

    +Se agregan la carga academica para guardar + validacion. +Se actualiza la variable de los turnos en base a la carga para que esta sea devuelta.

--- SWIPCA-UNI-API/DataAccess/DA_CargaAcademica.cs
+++ SWIPCA-UNI-API/DataAccess/DA_CargaAcademica.cs
@@ -38,46 +38,42 @@ namespace SWIPCA_UNI_API.DataAccess
         }
         public async Task<List<string>> ObtenerCargaAcademicaDocente(int IdDocente)
         {
-            var CargaAcademica1 = await (from carga in db.CargaAcademicas
-                                        join docente in db.Docentes
-                                        on carga.IdDocente equals docente.IdDocente
-                                        join clase in db.Clases
-                                        on carga.IdClase equals clase.IdClase
-                                        join grupo in db.Grupos
-                                        on carga.IdGrupo equals grupo.IdGrupo
-                                        join asignatura in db.Asignaturas
-                                        on clase.IdAsignatura equals asignatura.IdAsignatura
-                                        join carrera in db.Carreras
-                                        on carga.IdCarrera equals carrera.IdCarrera
-                                        join facultad in db.Facultads
-                                        on carrera.IdFacultad equals facultad.IdFacultad
-                                        join Aula_Lab in db.AulaLaboratorios
-                                        on facultad.IdFacultad equals Aula_Lab.IdFacultad
-                                        where carga.IdDocente == IdDocente && carga.Estado == 0
-                                        select $"{carga.IdCaHo}{asignatura.Nombre}{Aula_Lab.Nombre}{grupo.Nombre}"
-                                        ).ToListAsync();
+            var turnos = await db.Turnos.ToListAsync(); // Obtener la lista de turnos
 
-            var CargaAcademica2 = await (from carga in db.CargaAcademicas
-                                   join docente in db.Docentes
-                                   on carga.IdDocente equals docente.IdDocente
-                                   join clase in db.Clases
-                                   on carga.IdClase equals clase.IdClase
-                                   join grupo in db.Grupos
-                                   on carga.IdGrupo equals grupo.IdGrupo
-                                   join asignatura in db.Asignaturas
-                                   on clase.IdAsignatura equals asignatura.IdAsignatura
-                                   join carrera in db.Carreras
-                                   on carga.IdCarrera equals carrera.IdCarrera
-                                   join facultad in db.Facultads
-                                   on carrera.IdFacultad equals facultad.IdFacultad
-                                   join Aula_Lab in db.AulaLaboratorios
-                                   on facultad.IdFacultad equals Aula_Lab.IdFacultad
-                                   where carga.IdDocente == IdDocente && carga.Estado == 0
-                                   select $"{carga.Observacion}{asignatura.Frecuencia}").ToListAsync();
+            var PRECargaAcademica = await (from carga in db.CargaAcademicas
+                                           join docente in db.Docentes
+                                           on carga.IdDocente equals docente.IdDocente
+                                           join clase in db.Clases
+                                           on carga.IdClase equals clase.IdClase
+                                           join grupo in db.Grupos
+                                           on carga.IdGrupo equals grupo.IdGrupo
+                                           join asignatura in db.Asignaturas
+                                           on clase.IdAsignatura equals asignatura.IdAsignatura
+                                           join carrera in db.Carreras
+                                           on carga.IdCarrera equals carrera.IdCarrera
+                                           join facultad in db.Facultads
+                                           on carrera.IdFacultad equals facultad.IdFacultad
+                                           join Aula_Lab in db.AulaLaboratorios
+                                           on facultad.IdFacultad equals Aula_Lab.IdFacultad
+                                           where carga.IdDocente == IdDocente && carga.Estado == 0
+                                           select $"{carga.IdCaHo}{asignatura.Nombre}{Aula_Lab.Nombre}{grupo.Nombre}{turnos.Single(t => t.IdTurno == grupo.IdTurno).Nombre}{carga.Observacion}{asignatura.Frecuencia}"
+                                            ).ToListAsync();
 
-            var CargaAcademica = CargaAcademica1.Concat(CargaAcademica2).ToList();
-
-            return CargaAcademica;
+            return PRECargaAcademica;
         }
+        public async Task GuardarCargaAcademica(CargaAcademica cargaAcademica)
+        {
+            if (cargaAcademica.IdCarrera == 0 ||
+                cargaAcademica.IdClase == 0 ||
+                cargaAcademica.IdGrupo == 0 ||
+                cargaAcademica.IdDocente == 0 ||
+                cargaAcademica.IdJefe == 0)
+            {
+                throw new ArgumentException("Debe ingresar todos los datos requeridos.");
+            }
+            await db.CargaAcademicas.AddAsync(cargaAcademica);
+            await db.SaveChangesAsync();
+        }
+
     }
 }
